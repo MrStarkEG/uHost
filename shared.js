5ec5930573388be459374c0e4c8fdc2e5c5f31b7
Ahmed Elshahat <58784045+MrStarkEG@users.noreply.github.com>
2023-11-06 18:41:00 +0200

    Add files via upload

---
+++ shared.js
@@ -0,0 +1,54 @@
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const modalNoButton = document.querySelector(".modal__action--negative");
+const selectPlanButtons = document.querySelectorAll(".plan button");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function () {
+      backdrop.classList.add("open");
+    }, 10);
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.style.display = "none";
+  setTimeout(function () {
+    backdrop.classList.remove("open");
+  }, 10);
+}
+
+toggleButton.addEventListener("click", function () {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function () {
+    backdrop.classList.add("open");
+  }, 10);
+});
